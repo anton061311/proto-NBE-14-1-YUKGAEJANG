83403602a098d89d499781b88674849be8170c8e
songfox00 <songfox00@gmail.com>
2026-08-24 17:53:31 +0900

    add ErrorResponse

--- backend/src/main/java/com/yukgaejang/cafemenu/global/exceptionHandler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/yukgaejang/cafemenu/global/exceptionHandler/GlobalExceptionHandler.java
@@ -1,21 +1,22 @@
 package com.yukgaejang.cafemenu.global.exceptionHandler;
 
+import com.yukgaejang.cafemenu.global.dto.ErrorResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
-import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ApiException.class)
-    public ResponseEntity<?> handleApiException(ApiException ex) {
+    public ResponseEntity<ErrorResponse> handleApiException(ApiException ex) {
         return ResponseEntity.status(ex.getStatus())
-                .body(Map.of(
-                        "code", ex.getCode(),
-                        "message", ex.getMessage(),
-                        "timestamp", Instant.now().toString()));
+                .body(new ErrorResponse(
+                        ex.getCode(),
+                        ex.getMessage(),
+                        Instant.now()
+                ));
     }
 }
