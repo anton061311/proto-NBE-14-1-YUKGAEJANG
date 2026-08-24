4ffa2cbb70f749d92ad8292e36acb7ca6b241ef1
Chocobe <kyw05171@gmail.com>
2026-08-24 18:16:05 +0900

    feat: add Order entity

--- backend/src/main/java/com/yukgaejang/cafemenu/domain/post/order/entity/Order.java
+++ backend/src/main/java/com/yukgaejang/cafemenu/domain/post/order/entity/Order.java
@@ -1,4 +1,56 @@
 package com.yukgaejang.cafemenu.domain.post.order.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Getter
 public class Order {
+
+    /**
+     * 주문 ID
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * 주문자 이메일
+     */
+    private String email;
+
+    /**
+     * 우편변호
+     */
+    private String zipCode;
+
+    /**
+     * 배송 주소
+     */
+    private String address;
+
+    /**
+     * 주문 일시
+     */
+    private LocalDateTime orderDate;
+
+    public Order(
+            String email,
+            String zipCode,
+            String address,
+            LocalDateTime orderDate
+    ) {
+        this.email = email;
+        this.zipCode = zipCode;
+        this.address = address;
+        this.orderDate = orderDate;
+    }
+
 }
