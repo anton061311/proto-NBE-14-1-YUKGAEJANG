453d3877f68dc118c67d731e36ac0128a7479a97
songfox00 <songfox00@gmail.com>
2026-08-24 17:30:52 +0900

    feat: implements ApiException

--- backend/src/main/java/com/yukgaejang/cafemenu/global/exceptionHandler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/yukgaejang/cafemenu/global/exceptionHandler/GlobalExceptionHandler.java
@@ -1,15 +1,21 @@
 package com.yukgaejang.cafemenu.global.exceptionHandler;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ApiException.class)
-    public ResponseEntity<String> handleApiException(ApiException exception) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    public ResponseEntity<?> handleApiException(ApiException ex) {
+        return ResponseEntity.status(ex.getStatus())
+                .body(Map.of(
+                        "code", ex.getCode(),
+                        "message", ex.getMessage(),
+                        "timestamp", Instant.now().toString()));
     }
 }
