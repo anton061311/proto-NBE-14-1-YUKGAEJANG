fcf4a053557d6dd1e31260a058ea42a6b0e7a742
Chocobe <kyw05171@gmail.com>
2026-08-24 18:31:01 +0900

    feat: refactor OrderDto to use record for improved data handling

--- backend/src/main/java/com/yukgaejang/cafemenu/domain/post/order/dto/OrderDto.java
+++ backend/src/main/java/com/yukgaejang/cafemenu/domain/post/order/dto/OrderDto.java
@@ -1,4 +1,23 @@
 package com.yukgaejang.cafemenu.domain.post.order.dto;
 
-public class OrderDto {
+import com.yukgaejang.cafemenu.domain.post.order.entity.Order;
+
+import java.time.LocalDateTime;
+
+public record OrderDto(
+        String email,
+        String zipCode,
+        String address,
+        LocalDateTime orderDate
+) {
+
+    public static OrderDto from(Order order) {
+        return new OrderDto(
+                order.getEmail(),
+                order.getZipCode(),
+                order.getAddress(),
+                order.getOrderDate()
+        );
+    }
+
 }
