d4bdb92c916b68b63532d78c03cf2c1e1e1e0130
anton <anton061311@gmail.com>
2026-08-25 10:19:39 +0900

    refactor: introduce JpaREpository

---
+++ backend/src/main/java/com/yukgaejang/cafemenu/domain/post/order/repository/OrderItemRepository.java
@@ -0,0 +1,7 @@
+package com.yukgaejang.cafemenu.domain.post.order.repository;
+
+import com.yukgaejang.cafemenu.domain.post.order.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
