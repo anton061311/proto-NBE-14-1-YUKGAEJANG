ce15b82a0bb63278f2581a59a53e0afe9362f2b9
hanjongyeon644 <hanjongyeon644@gmail.com>
2026-08-25 11:16:36 +0900

    feat: implement product list

--- backend/src/main/java/com/yukgaejang/cafemenu/domain/post/product/controller/ProductController.java
+++ backend/src/main/java/com/yukgaejang/cafemenu/domain/post/product/controller/ProductController.java
@@ -1,7 +1,26 @@
 package com.yukgaejang.cafemenu.domain.post.product.controller;
 
+import com.yukgaejang.cafemenu.domain.post.product.dto.ProductResponse;
+import com.yukgaejang.cafemenu.domain.post.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public List<ProductResponse> getProducts() {
+        List<ProductResponse> products =
+                productService.getProducts();
+
+        return products;
+    }
 }
