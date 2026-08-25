05c9ea8c4791588be3545fc044802d499ef11275
Chocobe <kyw05171@gmail.com>
2026-08-25 14:51:50 +0900

    feat: add New Products and Order List pages, rename Home function to HomePage

---
+++ frontend/app/admin/products/add/page.tsx
@@ -0,0 +1,7 @@
+export default function NewProductsPage() {
+  return (
+    <div>
+      관지자 상품 등록 페이지
+    </div>
+  );
+}
---
+++ frontend/app/orders/page.tsx
@@ -0,0 +1,7 @@
+export default function OrderListPage() {
+  return (
+    <div>
+      주문 목록 페이지
+    </div>
+  );
+}
