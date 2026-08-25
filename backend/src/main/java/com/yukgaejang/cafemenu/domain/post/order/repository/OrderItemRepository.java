41596a0a77483f6b12a1233f99c9e0014ffae071
anton <anton061311@gmail.com>
2026-08-25 10:24:25 +0900

    feat: add items method

--- backend/src/main/java/com/yukgaejang/cafemenu/domain/post/order/repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yukgaejang.cafemenu.domain.post.order.repository;
-
-import com.yukgaejang.cafemenu.domain.post.order.entity.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
