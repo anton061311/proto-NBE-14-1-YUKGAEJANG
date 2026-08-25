6862315adc48f27799e8bb4c589552102bfba666
ljh2002ljh <aeb1119@naver.com>
2026-08-25 10:49:38 +0900

    feat: implement product crud

--- backend/src/main/java/com/yukgaejang/cafemenu/domain/post/product/controller/ProductController.java
+++ backend/src/main/java/com/yukgaejang/cafemenu/domain/post/product/controller/ProductController.java
@@ -1,7 +1,50 @@
 package com.yukgaejang.cafemenu.domain.post.product.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.yukgaejang.cafemenu.domain.post.product.dto.ProductCreateRequest;
+import com.yukgaejang.cafemenu.domain.post.product.dto.ProductResponse;
+import com.yukgaejang.cafemenu.domain.post.product.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/products")
 public class ProductController {
+
+    private final ProductService productService;
+
+
+    //create 상품 등록
+    @PostMapping
+    public ProductResponse createProduct(
+            @Valid @RequestBody ProductCreateRequest request
+    ) {
+        return productService.create(request);
+    }
+
+    //단건 상품 조회
+    @GetMapping("/{id}")
+    public ProductResponse getProduct(@PathVariable Long id) {
+        return productService.getProduct(id);
+    }
+
+
+    //전체 상품 조회
+    @GetMapping
+    public List<ProductResponse> getProducts() {
+        return productService.getProducts();
+    }
+
+
+    //상품 수정(update)
+    @PutMapping("/{id}")
+    public ProductResponse updateProduct(
+            @PathVariable Long id,
+            @Valid @RequestBody ProductCreateRequest request
+    ) {
+        return productService.updateProduct(id, request);
+    }
 }
--- backend/src/main/java/com/yukgaejang/cafemenu/domain/post/product/service/ProductService.java
+++ backend/src/main/java/com/yukgaejang/cafemenu/domain/post/product/service/ProductService.java
@@ -1,7 +1,68 @@
 package com.yukgaejang.cafemenu.domain.post.product.service;
 
+import com.yukgaejang.cafemenu.domain.post.product.dto.ProductCreateRequest;
+import com.yukgaejang.cafemenu.domain.post.product.dto.ProductResponse;
+import com.yukgaejang.cafemenu.domain.post.product.entity.Product;
+import com.yukgaejang.cafemenu.domain.post.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+
+    //상품 등록용(create)
+    public ProductResponse create(ProductCreateRequest request) {
+        Product product = new Product(
+                request.name(),
+                request.price(),
+                request.imageUrl()
+        );
+
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.from(savedProduct);
+
+    }
+
+    //단건 조회(read)
+    public ProductResponse getProduct(Long id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("상품이 존재하지 않습니다."));
+
+        return ProductResponse.from(product);
+    }
+
+    //전체 상품 조회(read)
+    public List<ProductResponse> getProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(ProductResponse::from)
+                .toList();
+    }
+
+    //상품 수정(update)
+    public ProductResponse updateProduct(Long id, ProductCreateRequest request) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("상품이 존재하지 않습니다."));
+
+        product.update(
+                request.name(),
+                request.price(),
+                request.imageUrl()
+        );
+
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.from(savedProduct);
+    }
 }
+
+
+
+
